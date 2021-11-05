f61edff38bf53f12530c66e7c0478950ea4f4557
Rodolfo Hernandez <79467082+Rhernandez14@users.noreply.github.com>
2021-11-05 16:04:51 -0700

    Create Helloworld.py

---
+++ Helloworld.py
@@ -0,0 +1,8 @@
+import sense_hat
+from time import sleep,time
+from random import randint
+sense = sense_hat.SenseHat()
+sense.clear()
+from sense_hat import SenseHat 
+sense = SenseHat() 
+sense.show_message("Hello world")
